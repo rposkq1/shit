6815f5efda029778c34033ecd8ceb4e96230fabc
rposkq1 <rposkq@proton.me>
2025-05-22 20:10:57 +0200

    ubuntu script that installs dependencies of dotfiles on ubuntu

---
+++ ubuntu-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt update && sudo apt install build-essential libx11-dev libxft-dev libxinerama-dev x11-xserver-utils xfonts-terminus xbindkeys unclutter-xfixes x11-utils xkbset xdotool
+
